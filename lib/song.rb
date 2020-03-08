82b5c00749eba77737459330da50bd5ffa90e3b4
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-08 01:02:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,13 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
-  def initialize
+  def initialize(name)
     @name = name 
     @@all << self
+  end 
+  
+  def name
+    @name 
   end 
 
   def self.all
